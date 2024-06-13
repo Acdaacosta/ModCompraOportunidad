81c1c3703e30120af1c89aa44b1f94963ead8309
Acdaacosta <daacosta@alimentoscarnicos.com.co>
2024-06-13 13:00:07 -0500

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,20 +6,7 @@ from ModCompraModelo import ModCompraModelo
 def main():
     controlador=ModCompraControlador()
     controlador.main()
-"""
-def test():
-    print("inicio")
-    pConsumo_pn = pd.read_csv(r'C:\Users\ACdaacosta\Downloads\DatosPrueba.csv',delimiter=';')
 
-    print("longitud "+str(pConsumo_pn.shape[1]))
-    edited_df = st.data_editor(pConsumo_pn)
-    print("longitud2 "+str(len(edited_df)))
-    clase=ModCompraModelo()
-
-    clase.optimizar(17,120000,1200000,10000,15000,120000,10707,edited_df)
-    
-    print("termino")
- """
 if __name__ == '__main__':
     main()
 
